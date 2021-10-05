cb621c241ca70a9d79d76bb27849e708f6589fab
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2021-10-05 20:14:45 +0200

    add filtering

--- models/searchParams/body.js
+++ models/searchParams/body.js
@@ -4,6 +4,10 @@ module.exports = {
         searchQuery: {type: 'string', nullable: true},
         zipcode: {type: 'string', nullable: true},
         page: {type: 'number', nullable: true},
-        categories: {type: 'array', default: []}
+        categories: {type: 'array', default: []},
+        sectors: {type: 'array', default: []},
+        regions: {type: 'array', default: []},
+        shiftTypes: {type: 'array', default: []},
+        educationLevels: {type: 'array', default: []},
     }
 }
